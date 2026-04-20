dc502284d7f94fd34719dc78f485b8bedce1cf5d
Anushka <dhawasanushka9@gmail.com>
2026-04-20 19:18:51 +0530

    fix: add @Entity to Activity for JPA

--- activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -1,51 +1,34 @@
 package com.fitness.activityservice.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
-
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
+@Entity   // ✅ MUST HAVE
 @Table(name = "activities")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Activity {
-    @Id
+
+    @Id   // ✅ JPA ID
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String userId;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Enumerated(EnumType.STRING)
     private ActivityType type;
 
     private Integer duration;
     private Integer caloriesBurned;
-
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startTime;
 
     @Column(columnDefinition = "TEXT")
     private String additionalMetrics;
 
-    @CreatedDate
     private LocalDateTime createdAt;
-
-    @LastModifiedDate
     private LocalDateTime updatedAt;
-
-
-
 }
